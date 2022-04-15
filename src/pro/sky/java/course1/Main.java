a8b8802e1296ade82ef2a71f35390603db11a957
Elshad <102314315+Elshad2192@users.noreply.github.com>
2022-04-15 09:02:38 +0500

    1

---
+++ src/pro/sky/java/course1/Main.java
@@ -0,0 +1,28 @@
+package pro.sky.java.course1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=0;
+        while (a!=10){
+            a++;
+            System.out.print(a+ " ");
+        }
+        System.out.println();
+        for (;a>0;a--)
+            System.out.print(a+" ");
+        //Задание 2
+        System.out.println();
+        int friday =1;
+        for (int i=friday; i<31;i+=7)
+            System.out.println("Сегодня пятница "+i+" число");
+
+        //Задание3
+        int currentYear=2022;
+        for (int i =0; i<currentYear+100;i+=79){
+            if (i>currentYear-200)
+                System.out.println(i);
+        }
+
+    }
+}
